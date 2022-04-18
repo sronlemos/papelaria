27bb0549327c617019e103d3c42725343748280c
sronLemos <sron.lemos@gmail.com>
2022-04-18 15:24:51 -0300

    Created project estoque

---
+++ estoque-api/src/test/java/com/github/sronlemos/estoqueapi/EstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sronlemos.estoqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
