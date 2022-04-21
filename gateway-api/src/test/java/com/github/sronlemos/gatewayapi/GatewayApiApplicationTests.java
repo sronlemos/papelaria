17e3ee443ec996e12e89619f393decee1134cad4
sronlemos <sron.lemos@gmail.com>
2022-04-21 11:28:42 -0300

    created project gateway-api

---
+++ gateway-api/src/test/java/com/github/sronlemos/gatewayapi/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sronlemos.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
