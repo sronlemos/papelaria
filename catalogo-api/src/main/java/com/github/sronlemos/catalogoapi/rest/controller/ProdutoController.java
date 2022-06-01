d5b6a166b7658d0e681bbceefd588f7d2c4e0307
sronlemos <sron.lemos@gmail.com>
2022-06-01 16:31:31 -0300

    incluido zipkin

--- catalogo-api/src/main/java/com/github/sronlemos/catalogoapi/rest/controller/ProdutoController.java
+++ catalogo-api/src/main/java/com/github/sronlemos/catalogoapi/rest/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package com.github.sronlemos.catalogoapi.rest.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 public class ProdutoController {
 	
+	private Logger logger = LoggerFactory.getLogger(ProdutoController.class); 
+	
 	@Autowired
 	private Environment environment;
 	
@@ -28,6 +32,7 @@ public class ProdutoController {
 	@GetMapping("/produtos")
 	public List<Produto> findAll() {
 		
+		logger.info("findAll produtos");
 		String porta = environment.getProperty("local.server.port");
 		System.out.println(porta);
 		return produtoRepository.findAll();
